a0ce2649f16acc98aff9f0197dad187762ae7106
Voemushroom <43127596+Voemushroom@users.noreply.github.com>
2019-10-18 10:59:10 +0900

    Update ALIS_OAuth.php

--- ALIS_OAuth/ALIS_OAuth.php
+++ ALIS_OAuth/ALIS_OAuth.php
@@ -8,10 +8,12 @@ function base64url_encode($data) {
 //ランダムに数字を生成
 $length = mt_rand(111111,999999);
 
+//登録したアプリケーションのidをセット
 $client_id = "xxxxx";
 $redirect_uri = "http://voemushroom.com/alis_code.php";
 $scope = "read";
 
+//生成
 $code_verifier  = base64url_encode(substr(bin2hex(random_bytes($length)), 0, 50));
 $code_challenge = base64url_encode( hash ('sha256',$code_verifier, true));
 
@@ -23,7 +25,7 @@ $_SESSION["code_verifier"] = $code_verifier;
 
 $url = "https://alis.to/oauth-authenticate?client_id=".$client_id."&redirect_uri=".$redirect_uri."&scope=".$scope."&code_challenge=".$code_challenge."&code_challenge_method=".$code_challenge_method."&response_type=".$response_type;
 
-//認証画面へ
+//認証画面へジャンプ
 header("Location: ".$url);
 exit();
 
